76763e9ff2b8f05775f37bc20d8b7b9f8fe43d47
Danilo Cândido <danilod07@gmail.com>
2018-05-10 13:39:59 -0300

    restaurant.txt created

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -7,9 +7,24 @@ class Restaurant
   end
 
   def self.file_exists?
+    if @@filepath && File.exists?(@@filepath)
+      true
+    else
+      false
+    end
+  end
+
+  def self.file_usable?
+    return false unless @@filepath
+    return false unless File.exists?(@@filepath)
+    return false unless File.readable?(@@filepath)
+    return false unless File.writable?(@@filepath)
+    return true
   end
 
   def self.create_file
+    File.open(@@filepath, 'w') unless file_exists?
+    return file_usable?
   end
 
   def self.saved_restaurants
